ec3fd066406cf744ff921bdecb3bf1c3b5e26b26
Joseph <joseph.mikhail.developer@gmail.com>
2022-10-11 13:35:37 +0200

    fix

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aceee33056a594cbdb3db9130e92c7dd-548620125.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
